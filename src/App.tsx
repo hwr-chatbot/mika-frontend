d37e032cb88e86cdb0cd051628a919dcc00bd57e
Floxyi <florianmeik.winkler@gmail.com>
2025-06-13 18:45:46 +0200

    improve entry points

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,33 @@
-import "./App.css"
-import Main from "./components/Main"
-import Header from "./components/Header/Header"
-import Footer from "./components/Footer/Footer"
+import { Routes, Route } from "react-router-dom";
 
+import "./App.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
+import Chat from "./components/pages/Chat/Chat";
+import About from "./components/pages/About/About";
+import Feedback from "./components/pages/Feedback/Feedback";
+import Imprint from "./components/pages/Imprint/Imprint";
+import DataSecurity from "./components/pages/DataSecurity/DataSecurity";
 
 function App() {
-	return (
-		<div className="app">
-			     <Header />
-				 <Main />
-				 <Footer />
-		</div>
-	)
+    return (
+        <div className="app">
+            <Header />
+
+            <main>
+                <Routes>
+                    <Route path="/" element={<Chat host="localhost" port={5505} useHttps={true} />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/feedback" element={<Feedback />} />
+                    <Route path="/imprint" element={<Imprint />} />
+                    <Route path="/data-security" element={<DataSecurity />} />
+                </Routes>
+            </main>
+
+            <Footer />
+        </div>
+    );
 }
 
-export default App
+export default App;
