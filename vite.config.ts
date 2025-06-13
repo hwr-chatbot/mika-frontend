babd8da316d7433f82d207163a4a9595e48a9432
Floxyi <florianmeik.winkler@gmail.com>
2025-06-13 19:08:00 +0200

    fix preview

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,23 @@
 /// <reference types="vitest" />
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from "tailwindcss"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
 
 export default defineConfig({
     plugins: [react()],
     css: {
-      postcss: {
-        plugins: [tailwindcss()],
-      },
+        postcss: {
+            plugins: [tailwindcss()],
+        },
     },
     test: {
-      globals: false,
-      environment: "jsdom"
+        globals: false,
+        environment: "jsdom",
     },
-})
+    preview: {
+        allowedHosts: ["mika.lehre.hwr-berlin.de"],
+    },
+    server: {
+        allowedHosts: ["mika.lehre.hwr-berlin.de"],
+    },
+});
